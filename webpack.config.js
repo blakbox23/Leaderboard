fe685912d36eb1c75ab6d7b8b92973723f29dfed
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-08-31 11:21:53 +0300

    Fix linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -4,24 +4,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-   devServer: {
-       contentBase: './dist',
-     },
+  devServer: {
+    contentBase: './dist',
+  },
   plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-      ],
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-        ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
+    ],
+  },
 };
